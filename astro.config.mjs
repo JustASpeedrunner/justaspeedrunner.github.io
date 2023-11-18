3f399ee4f2163f6d2a9ec5f91f2df7bc181c1f91
JustASpeedrunner <sirgamesalot32706@gmail.com>
2023-11-18 01:23:38 -0600

    update for github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,5 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify/functions";
-import robotsTxt from "astro-robots-txt";
-import UnoCSS from "@unocss/astro";
+import { defineConfig } from 'astro/config'
 
-import solidJs from "@astrojs/solid-js";
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarco.xyz/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-  ],
-  output: "server",
-  adapter: netlify(),
-});
+  site: 'https://justaspeedrunner.github.io',
+})
