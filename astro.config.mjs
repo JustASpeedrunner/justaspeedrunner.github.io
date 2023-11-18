65055e6e09b0cc435575309a359dca1d8afd84ee
JustASpeedrunner <sirgamesalot32706@gmail.com>
2023-11-18 01:29:26 -0600

    unfuck up (hopefully)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify/functions";
+import robotsTxt from "astro-robots-txt";
+import UnoCSS from "@unocss/astro";
 
+import solidJs from "@astrojs/solid-js";
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://justaspeedrunner.github.io',
-})
+  site: "https://justaspeedrunner.github.io",
+  integrations: [
+    sitemap(),
+    robotsTxt({
+      sitemap: [
+        "https://justaspeedrunner.github.io/sitemap-index.xml",
+        "https://justaspeedrunner.github.io/sitemap-0.xml",
+      ],
+    }),
+    solidJs(),
+    UnoCSS({ injectReset: true }),
+  ],
+  output: "server",
+  adapter: netlify(),
+});
